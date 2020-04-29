46ae54037a3f8af2d530f08ad67deb2cfe221247
nitiza <nitishaagarwal3@gmail.com>
2020-04-29 10:32:08 +0530

    add task to the todo list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes , RouterModule } from '@angular/router';
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
 import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const ROUTES: Routes =[
   { path: "", component: HomeComponent},
@@ -19,7 +22,10 @@ const ROUTES: Routes =[
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    NgbModule,
+    NgbPaginationModule, NgbAlertModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
